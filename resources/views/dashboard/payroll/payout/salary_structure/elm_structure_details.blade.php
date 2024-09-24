a67742ec8588c460428ff0d050a920ae029c08be
aswanesher <aswansetiawan01@gmail.com>
2024-09-24 14:22:38 +0700

    feat : adjust elm salary structures

--- resources/views/dashboard/payroll/payout/salary_structure/elm_structure_details.blade.php
+++ resources/views/dashboard/payroll/payout/salary_structure/elm_structure_details.blade.php
@@ -4,10 +4,20 @@
     @csrf
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-5 mt-2">
             <x-form.input id="structure_name" label="Name" name="structure_name" required />
-            <x-form.input id="company" label="Company" name="company" required />
-            <x-form.input id="currency" label="Currency" name="currency" required />
-            <x-form.input id="leave_head" label="Leave Head" name="leave_head" required />
-            
+            <x-form.select id="company_id" name="company_id" label="Company" url="{{ url('dashboard/hrms/designation') }}"
+                           apiUrl="{{ $apiCompanyUrl }}/company/datatables" columns='["company_name"]' :selected="$company"
+                           :keys="[
+                    'company_id' => $company,
+                ]">
+                <option value="">Select Company</option>
+            </x-form.select>
+            <x-form.select name="currency" id="currency" label="Currency" class="tom-select w-full"
+                           data-placeholder="Select Currency" url="{{ url('dashboard/hrms/designation') }}" required>
+                <option value="">Select Currency</option>
+                <option value="male">Dollar</option>
+                <option value="female">Indonesian Rupiah</option>
+            </x-form.select>
+
             <x-checkbox id="is_active"
             label="Is Active"
             name="is_active"
@@ -18,9 +28,10 @@
         <x-form.input id="max_benefit" label="Max Benefit (IDR)" name="max_benefit" required />
         <x-form.select name="payroll_frequency" id="payroll_frequency" label="Payroll Frequency" class="tom-select w-full"
             data-placeholder="Select Frequency" url="{{ url('dashboard/hrms/designation') }}" required>
-            <option value="male">Select Frequency</option>
-            <option value="male">Mal</option>
-            <option value="female">Female</option>
+            <option value="">Select Frequency</option>
+            <option value="monthly">Monthly</option>
+            <option value="weekly">Weekly</option>
+            <option value="daily">Daily</option>
         </x-form.select>
         <x-checkbox id="is_salary_slip_based_on_timesheet"
             label="Salary Slip Based on Timesheet"
