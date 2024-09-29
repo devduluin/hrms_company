58237f7f9d69ac0605dfd2f497072e2c7317df5c
aswanesher <aswansetiawan01@gmail.com>
2024-09-29 20:17:31 +0700

    feat : fix route

--- routes/attendance.php
+++ routes/attendance.php
@@ -4,6 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('/attendance')->group(function () {
+    Route::controller(AttendanceController::class)->group(function () {
+        Route::get('/', 'index')->name('attendance');
+        Route::get('/summary', 'summary')->name('summary');
+        Route::get('/detail/{id}', 'detail')->name('detail');
+        Route::get('/shift_assignment', 'shift')->name('shift');
+        Route::get('/shift_list', 'shift_list')->name('shift_list');
+        Route::get('/report', 'report')->name('report');
+        Route::get('/new_shift_assignment', 'new_assignment')->name('shift');
+    });
+});
+
 //leave modules
 Route::prefix('/leave')->group(function () {
     Route::controller(LeaveController::class)->group(function () {
--- routes/payroll.php
+++ routes/payroll.php
@@ -4,17 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Route;
 
-//Route::prefix('/attendance')->group(function () {
-//    Route::controller(AttendanceController::class)->group(function () {
-//        Route::get('/', 'index')->name('attendance');
-//        Route::get('/summary', 'summary')->name('summary');
-//        Route::get('/detail/{id}', 'detail')->name('detail');
-//        Route::get('/shift_assignment', 'shift')->name('shift');
-//        Route::get('/shift_list', 'shift_list')->name('shift_list');
-//        Route::get('/report', 'report')->name('report');
-//    });
-//});
-
 // claim modules
 Route::prefix('/claim')->group(function () {
     Route::controller(ClaimController::class)->group(function () {
