0b5c8a4f897c2897b7946640297726612e6cb9dc
aswanesher <aswansetiawan01@gmail.com>
2024-12-17 15:41:18 +0700

    image upload and delete in expense claim

--- resources/views/dashboard/hrms/attendance/activity/index.blade.php
+++ resources/views/dashboard/hrms/attendance/activity/index.blade.php
@@ -17,7 +17,7 @@
                     <div data-tw-merge="" data-tw-placement="bottom-end" class="dropdown relative inline-block"><button data-tw-merge="" data-tw-toggle="dropdown" aria-expanded="false" class="transition duration-200 border inline-flex items-center justify-center py-2 px-3 rounded-md font-medium cursor-pointer focus:ring-4 focus:ring-primary focus:ring-opacity-20 focus-visible:outline-none dark:focus:ring-slate-700 dark:focus:ring-opacity-50 [&amp;:hover:not(:disabled)]:bg-opacity-90 [&amp;:hover:not(:disabled)]:border-opacity-90 [&amp;:not(button)]:text-center disabled:opacity-70 disabled:cursor-not-allowed bg-secondary/70 border-secondary/70 text-slate-500 dark:border-darkmode-400 dark:bg-darkmode-400 dark:text-slate-300 [&amp;:hover:not(:disabled)]:bg-slate-100 [&amp;:hover:not(:disabled)]:border-slate-100 [&amp;:hover:not(:disabled)]:dark:border-darkmode-300/80 [&amp;:hover:not(:disabled)]:dark:bg-darkmode-300/80 shadow-md  w-full sm:w-auto"><i data-tw-merge="" data-lucide="arrow-down-wide-narrow" class="mr-2 h-4 w-4 stroke-[1.3]"></i>
                         Filter
                         <span id="countFilter" class="ml-2 flex h-5 items-center justify-center rounded-full border bg-slate-100 px-1.5 text-xs font-medium">
-                            
+
                         </span></button>
                         <div data-transition="" data-selector=".show" data-enter="transition-all ease-linear duration-150" data-enter-from="absolute !mt-5 invisible opacity-0 translate-y-1" data-enter-to="!mt-1 visible opacity-100 translate-y-0" data-leave="transition-all ease-linear duration-150" data-leave-from="!mt-1 visible opacity-100 translate-y-0" data-leave-to="absolute !mt-5 invisible opacity-0 translate-y-1" class="dropdown-menu absolute z-[9999] hidden">
                             <div data-tw-merge="" class="dropdown-content rounded-md border-transparent bg-white p-2 shadow-[0px_3px_10px_#00000017] dark:border-transparent dark:bg-darkmode-600">
@@ -96,14 +96,14 @@
             if (data !== null) {
                 const name = data.first_name + ' ' + data.last_name;
                 return `<a class="text-primary font-medium" href="{{ url("dashboard/hrms/attendance/activity/employee") }}/${row.id}">${name}</a>`;
-                 
+
             }
             return 'N/A';
         }
 
         function getAttachment(data, type, row, meta) {
             if (data) {
-               
+
                 return `<a target="_blank" class="text-primary font-medium" href="${data}">Download</a>`;
             }
             return 'N/A';
@@ -117,7 +117,7 @@
                 year: 'numeric',
                 month: '2-digit',
                 day: '2-digit',
-             
+
             };
 
             return utcDate.toLocaleString('en-GB', options); // Combined date and time
@@ -129,11 +129,11 @@
             <i class="fa-solid fa-list text-base"></i></button>
                 <div data-transition data-selector=".show" data-enter="transition-all ease-linear duration-150" data-enter-from="absolute !mt-5 invisible opacity-0 translate-y-1" data-enter-to="!mt-1 visible opacity-100 translate-y-0" data-leave="transition-all ease-linear duration-150" data-leave-from="!mt-1 visible opacity-100 translate-y-0" data-leave-to="absolute !mt-5 invisible opacity-0 translate-y-1" class="dropdown-menu absolute z-[9999] hidden">
                     <div data-tw-merge class="dropdown-content rounded-md border-transparent bg-white p-2 shadow-[0px_3px_10px_#00000017] dark:border-transparent dark:bg-darkmode-600 w-40">
-                       
-                         
+
+
                         <a onClick="action('detail', '`+data['id']+`')" class="cursor-pointer flex items-center p-2 transition duration-300 ease-in-out rounded-md hover:bg-slate-200/60 dark:bg-darkmode-600 dark:hover:bg-darkmode-400 dropdown-item"><i data-tw-merge data-lucide="external-link" class="stroke-[1] w-5 h-5 w-4 h-4 mr-2 w-4 h-4 mr-2"></i>
                             Detail</a>
-                       
+
                     </div>
                 </div>
             </div>`;
@@ -160,7 +160,7 @@
             }else{
                 location.href = '{{ url("/dashboard/hrms/attendance/activity") }}/'+action+'/'+id;
             }
-            
+
         }
 
         async function destroy(method, path){
@@ -197,7 +197,7 @@
             $(`#employee_id`)[0].tomselect.clear();
             $(`#attendance_date`).val("");
         }
-        
+
         $(document).ready(function () {
             const urlParams = new URLSearchParams(window.location.search);
             let activeFilterCount = 0;
